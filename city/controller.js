6072bc2f7080f7997e6676968bc6d08eaba71570
ぐらむ/gramme <127509194+gramme-linkcom@users.noreply.github.com>
2023-12-05 00:30:31 +0900

    Delete city directory

--- city/controller.js
+++
@@ -1,9 +0,0 @@
-$(".MenuButton").click(function () {
-	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#navi").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-});
-
-$("#navi a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".MenuButton").removeClass('active');//ボタンの activeクラスを除去し
-    $("#navi").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
-});
--- city/logout.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", function(){
-    Cookies.remove("Password");
-    Cookies.remove("UserName");
-    window.location.href = './index';
-});
