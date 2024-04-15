a5fe737bc7a4fdf6356f4b4ccf609958b8174730
ぐらむ/gramme <127509194+gramme-linkcom@users.noreply.github.com>
2024-04-15 23:27:31 +0900

    Delete city directory

--- city/link.js
+++
@@ -1,9 +0,0 @@
-window.onload = onLoad;
-
-function onLoad() {
-    link = document.getElementById('base');
-    let root = "gramme-linkcom.github.io"
-    link.setAttribute('href', "http://"+root);
-
-    console.log("completed rewrite")
-}
